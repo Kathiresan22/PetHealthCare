6df0fba494b2b7b8e746d232bead862e4e82e665
Kathiresan22 <arukathiresan@gmail.com>
2018-10-12 19:42:54 +0530

    Separate Files
    Css and js used in PetInfo page moved to separed files

---
+++ js/petinfo.js
@@ -0,0 +1,14 @@
+ $(document).ready(function(){
+  		$('.modal').modal();
+ });
+ 
+ $(document).ready(function(){
+    $('.tabs').tabs();
+	$('.dropdown-trigger').dropdown();
+	$(".dropdown-content>li>a").css("color", "#EF9A9A");
+  });
+  
+   $(document).ready(function(){
+    $('.datepicker').datepicker();
+	
+  });
